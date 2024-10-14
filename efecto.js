3dfe9cd39a582fbf7304e147a35e6e70f687709c
EduardoGallardoR <egallardo821@gmail.com>
2024-10-13 20:11:46 -0600

    corecciones

--- efecto.js
+++ efecto.js
@@ -23,12 +23,12 @@ window.addEventListener('scroll', function() {
         const progress = Math.min(Math.max(scrollPercentage, 0), 1.1);
 
         // Ajustar la altura de la barra de progreso según el porcentaje de scroll
-        const maxTimelineHeight = 310; // Altura máxima de la línea de tiempo
+        const maxTimelineHeight = 505; // Altura máxima de la línea de tiempo
         progressBar.style.height = (progress * maxTimelineHeight) + 'px';
         
         // Si se está desplazando hacia abajo y la barra no está llena, llenarla
         if (!isFilled) {
-            progressBar.style.height = '300px'; // Mantener la barra llena
+            progressBar.style.height = '505px'; // Mantener la barra llena
             isFilled = true; // Actualizar el estado de la barra
         }
     } else if (window.scrollY < timelineRect.top && isFilled) {
